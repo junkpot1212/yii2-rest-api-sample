fb898ade2e84a58cec056439a6c26954744f6f70
Nobuhiro Ueda <ueda@tech-vein.com>
2021-12-18 16:27:20 +0900

    :+1: CORSの設定

--- src/controllers/MemberController.php
+++ src/controllers/MemberController.php
@@ -4,9 +4,27 @@ namespace app\controllers;
 
 
 use app\models\infrastructures\Members;
+use yii\filters\Cors;
 use yii\rest\ActiveController;
 
 class MemberController extends ActiveController
 {
+    public function behaviors()
+    {
+        return [
+            'corsFilter' => [
+                'class' => Cors::class,
+                'cors'  => [
+                    'Origin'                           => ['http://localhost:8000'],
+                    'Access-Control-Request-Method'    => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                    'Access-Control-Request-Headers'   => ['*'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age'           => 86400,
+                    'Access-Control-Expose-Headers'    => [],
+                ],
+            ],
+        ];
+    }
+
     public $modelClass = Members::class;
 }
